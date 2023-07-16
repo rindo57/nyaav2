99451fac3924cbb96cc307a642c036f8e415a1aa
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-07-16 10:32:52 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -102,6 +102,7 @@ async def start_uploading(data):
         trust = data['trust']
         cid = data['categoryid']
         category = data['category']
+        remake = data['remake']
         magnet = "https://nyaasi-to-magnet.up.railway.app/nyaamagnet/urn:btih:" + link
         clink = "https://nyss.si/?c=" + cid
         if remake=="Yes":
@@ -111,7 +112,7 @@ async def start_uploading(data):
         else:
             remake=remake.replace("No", "")
             trust=trust.replace("No", "")
-        xtext = f"**{title}**" + "\n" + f"{size}" + " | " + f"[Download]({dlink})" + " | " + f"[View]({vlink})" + remake + trust + "\n" + f"[#c{cid} {category}]({clink})" + "\n" + "\n" + f"[🔗 Magnet]({magnet})"
+        xtext = f"**{title}**" + "\n" + f"{size}" + " | " + f"[Download]({dlink})" + " | " + f"[View]({vlink})" + f"{remake}" + f"{trust}" + "\n" + f"[#c{cid} {category}]({clink})" + "\n" + "\n" + f"[🔗 Magnet]({magnet})"
         KAYO_ID = -1001900103251
         untext = await app.send_message(
                       chat_id=KAYO_ID,
