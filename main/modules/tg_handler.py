ebf9f54c9f675edd2f7c6094ab25185428317dbb
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-12-17 00:47:35 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -116,7 +116,7 @@ async def start_uploading(data):
                       chat_id=KAYO_ID,
                       text=xtext,
                       disable_web_page_preview=True,
-                      parse_mode=html
+                      parse_mode="html"
                   )
         xt = untext.message_id
         await asyncio.sleep(3)
