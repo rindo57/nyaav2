2fee5d7ee62684421e0fb75529b27c8380c22088
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-05 01:20:30 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -7,28 +7,31 @@ from uvloop import install
 from contextlib import closing, suppress
 from main.modules.tg_handler import tg_handler
 
-
+loop = asyncio.get_event_loop()
 
 @app.on_message(filters.command(["help","ping"]))
 async def start(bot, message: Message):
-  return await message.reply_text("⚡ **Bot Is up...**")
+    return await message.reply_text("⚡ Bot Is up...")
 
 async def start_bot():
-  print("==================================")
-  print("[INFO]: AutoAnimeBot Started Bot Successfully")
-  print("==========JOIN @Latest_ongoing_airing_animes=========")
+    print("==================================")
+    print("[INFO]: AutoAnimeBot Started Bot Successfully")
+    print("==========JOIN @Latest_ongoing_airing_animes=========")
 
-  print("[INFO]: Adding Parsing Task")
-  asyncio.create_task(auto_parser())
-  asyncio.create_task(tg_handler())
-  
-  await idle()
-  print("[INFO]: BOT STOPPED")
-  await app.stop()  
-  for task in asyncio.all_tasks():
-    task.cancel()
+    print("[INFO]: Adding Parsing Task")
+    asyncio.create_task(auto_parser())
+    asyncio.create_task(tg_handler())
+    
+    await idle()
+    print("[INFO]: BOT STOPPED")
+    await app.stop()  
+
+    for task in asyncio.all_tasks():
+        task.cancel()
+
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(start_bot())
-  
+    install()
+    with closing(loop):
+        with suppress(asyncio.exceptions.CancelledError):
+            loop.run_until_complete(start_bot())
+            await asyncio.sleep(3.0)
